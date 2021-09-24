618380079596e762b6f1e8fff61eff9d252e7b6a
ariellongoria <ariello97@hotmail.com>
2021-09-24 16:41:23 -0500

    initial files and folder structure

---
+++ server.js
@@ -0,0 +1,29 @@
+// import modules
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
+
+// start express
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// initialize handlebars
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// add middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log("Now listening"));
+});
